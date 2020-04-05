e08a10c4babf0d260ef047c24dce1f24a76cd8a3
datelloa9 <datelloa9@students.rowan.edu>
2020-04-04 21:21:28 -0400

    Employee Hours and Inventory intent
    Updated hours worked intent and added an Inventory seach intent

---
+++ AndroidAlexaSkillProject/app/src/main/java/com/example/androidalexaskillproject/SingleFragmentActivty.java
@@ -0,0 +1,120 @@
+package com.example.androidalexaskillproject;
+
+import android.content.Intent;
+import android.os.AsyncTask;
+import android.os.Bundle;
+import android.view.Menu;
+import android.view.MenuInflater;
+import android.view.MenuItem;
+import android.widget.ArrayAdapter;
+import android.widget.Spinner;
+import android.widget.Toast;
+
+import androidx.annotation.NonNull;
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.fragment.app.Fragment;
+import androidx.fragment.app.FragmentManager;
+
+import java.io.BufferedInputStream;
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.InputStreamReader;
+import java.net.HttpURLConnection;
+import java.net.MalformedURLException;
+import java.net.URL;
+import java.util.ArrayList;
+import java.util.concurrent.ExecutionException;
+
+public abstract class SingleFragmentActivty extends AppCompatActivity {
+    protected abstract Fragment createFragment();
+
+
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        FragmentManager fm = getSupportFragmentManager();
+        Fragment fragment = fm.findFragmentById(R.id.fragment_container);
+        if (fragment == null) {
+            fragment = createFragment();
+            fm.beginTransaction().add(R.id.fragment_container, fragment).commit();
+        }
+
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        MenuInflater inflater = getMenuInflater();
+        inflater.inflate(R.menu.main_menu, menu);
+        return true;
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(@NonNull MenuItem item) {
+        switch (item.getItemId()){
+
+            case R.id.drop_down_menu_list:
+                Toast.makeText(this, "drop down menu selected", Toast.LENGTH_SHORT).show();
+                return true;
+
+            case R.id.Profits_list:
+                //M.Marinaro 3/4/20 : By selecting this menu item the current sheet name is changed
+                SheetRepository.getInstance().setSheetProfits();
+                //M.Marinaro 3/4/20 : Then refresh the page
+                Intent ProfitListintent = new Intent(this, MainListActivity.class);
+                startActivity(ProfitListintent);
+                return true;
+
+            //M.Marinaro 3/4/20 : Same Here
+            case R.id.Expenses_list:
+                SheetRepository.getInstance().setSheetExpenses();
+                Intent ExpensesListIntent = new Intent(this, MainListActivity.class);
+                startActivity(ExpensesListIntent);
+                return true;
+
+
+            //M.Marinaro 3/9/20 : Option to select the employees list
+            case R.id.Employees_list:
+                SheetRepository.getInstance().setSheetEmployees();
+                Intent EmployeeListIntent = new Intent(this, MainListActivity.class);
+                startActivity(EmployeeListIntent);
+                return true;
+
+            case R.id.GrossProfit_list:
+                Intent GrossProfitListintent = new Intent(this, MainListGrossProfitActivity.class);
+                startActivity(GrossProfitListintent);
+                return true;
+
+
+            //M.Marinaro 3/9/20 : General Add Button
+            case R.id.Add_item:
+                Intent AddItemintent = new Intent(this, ProfitReturnAddFragment.class);
+                startActivity(AddItemintent);
+                return true;
+
+            case R.id.select_stats:
+                Toast.makeText(this, "stats selected", Toast.LENGTH_SHORT).show();
+                return true;
+
+            case R.id.Pie_chart:
+                Toast.makeText(this, "pie selected", Toast.LENGTH_SHORT).show();
+                Intent PieChartItent = new Intent(this, PieChartReturnFragment.class);
+                startActivity(PieChartItent);
+
+                return true;
+
+            case R.id.Bar_chart:
+                Toast.makeText(this, "pie selected", Toast.LENGTH_SHORT).show();
+                Intent BarChartItent = new Intent(this, BarChartReturnFragment.class);
+                startActivity(BarChartItent);
+
+                return true;
+
+                default:
+                    return super.onOptionsItemSelected(item);
+        }
+
+    }
+}
